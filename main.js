25a58fcd37532489713c8f452c5b855419dbee8e
USER <user@USERnoMacBook-ea.local>
2020-04-19 14:56:45 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+const array = [1, 2, 3, 4, 5, 6, 'moji', false ];
+
+arry.push('new item');
+console.log(arry, length);
+
+// 先頭に代入
+// array.unshift('new item');
+// 最後を削除
+// array.pop();
+// 削除された値を出力
+// const val = arry.pop();
+// 先頭を削除
+// const val = arry.shift();
